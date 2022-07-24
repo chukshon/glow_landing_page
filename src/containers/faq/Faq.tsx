05c61035f09d2663a75840dcebe4314bdb845e8a
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-07-24 23:52:06 +0100

    faq image added

--- src/containers/faq/Faq.tsx
+++ src/containers/faq/Faq.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Wrapper, StyledImg } from './style'
+import faqImg from '../../assets/images/faqImg.png'
 
 const Faq = () => {
-  return <div>Faq</div>
+  return (
+    <Wrapper>
+      <StyledImg>
+        <img src={faqImg} alt='' />
+      </StyledImg>
+    </Wrapper>
+  )
 }
 
 export default Faq
---
+++ src/containers/faq/style.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  padding: 30px 100px;
+`
+
+export const StyledImg = styled.div``
